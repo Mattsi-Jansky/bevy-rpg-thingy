ed29d922d7f528f2c0a784f641a009e7f8889024
Mattsi <mattsi.jansky@codurance.com>
2024-04-06 15:00:41 +0100

    Add systems namespace

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@ use bevy_scene_hook::HookPlugin;
 use bevy_mod_raycast::prelude::*;
 use character::CharacterBundle;
 use crate::assets::animations::{Animations, init_animations};
+use crate::camera::setup_camera;
 use crate::environment::render_environment;
+use crate::lighting::setup_lighting;
 use crate::map::MAP;
+use crate::systems::cursor::update_cursor;
 
 mod environment;
 mod map;
-mod cursor;
 mod character;
 mod camera;
 mod lighting;
 mod assets;
+mod systems;
 
 fn main() {
     App::new()
@@ -27,13 +30,13 @@ fn main() {
         .add_plugins(DefaultPlugins.set(bevy_mod_raycast::low_latency_window_plugin()))
         .add_systems(Startup, (init_meshes, init_animations, setup).chain())
         .add_systems(Update, setup_scene_once_loaded)
-        .add_systems(Update, cursor::update_cursor)
+        .add_systems(Update, update_cursor)
         .run();
 }
 
 fn setup(mut commands: Commands, meshes: Res<Meshes>) {
-    camera::setup_camera(&mut commands);
-    lighting::setup_lighting(&mut commands);
+    setup_camera(&mut commands);
+    setup_lighting(&mut commands);
     commands.spawn(CharacterBundle::new(&meshes));
     render_environment(&mut commands, &meshes, &MAP);
 }
