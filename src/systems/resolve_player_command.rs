6f71ef6e0d9e9d5345d26743fdef63cc5d47916d
Mattsi <mattsi.jansky@codurance.com>
2024-04-06 16:49:51 +0100

    Camera follows player movement

--- src/systems/resolve_player_command.rs
+++ src/systems/resolve_player_command.rs
@@ -1,18 +1,27 @@
-use bevy::prelude::{Entity, EventReader, Query, Transform};
+use bevy::prelude::{Camera, Camera3dBundle, Commands, Entity, EventReader, Query, Transform, Vec3, With, Without};
 use crate::character::CharacterState;
 use crate::environment::TILE_SIZE;
 use crate::events::NewPlayerCommand;
 
 pub fn resolve_player_commands(
+    mut commands: Commands,
     mut events: EventReader<NewPlayerCommand>,
-    mut character_query: Query<(Entity, &CharacterState, &mut Transform)>,
+    mut character_query: Query<&mut Transform, With<CharacterState>>,
+    mut camera_query: Query<(Entity, &mut Transform), (With<Camera>, Without<CharacterState>)>
 ) {
     for event in events.read() {
         match event {
             NewPlayerCommand::Move { x, z } => {
-                if let (_, _, mut transform) = character_query.single_mut() {
-                    transform.translation.x = (*x as f32) * TILE_SIZE;
-                    transform.translation.z = (*z as f32) * TILE_SIZE;
+                let world_x = (*x as f32) * TILE_SIZE;
+                let world_z = (*z as f32) * TILE_SIZE;
+                if let (mut transform) = character_query.single_mut() {
+                    transform.translation.x = world_x;
+                    transform.translation.z = world_z;
+                }
+
+                if let (entity, transform) = camera_query.single_mut() {
+                    commands.entity(entity).insert(Transform::from_xyz(10.0 + world_x, 10.0, 15.5 + world_z)
+                                                       .looking_at(Vec3::new(world_x, 1.0, world_z), Vec3::Y));
                 }
             }
         }
