f35da028dcd6ba997b28b5e1eed9b7eff47458aa
Mattsi <mattsi.jansky@codurance.com>
2024-04-07 21:20:10 +0100

    Remove some debug logs

--- src/systems/wall_opacity.rs
+++ src/systems/wall_opacity.rs
@@ -11,14 +11,8 @@ pub fn update_wall_opacity(
     children: Query<&Children>
 ) {
     for blocking_wall in query_blocking_walls.iter() {
-        info!("Found wall with blocking component");
         for child in children.iter_descendants(blocking_wall) {
-            info!("Found wall child");
             if let Ok(material_handle) = query_material.get(child) {
-                info!("Found material in wall child");
-                // let Some(material) = materials.get_mut(material_handle) else { continue; };
-                // material.alpha_mode = AlphaMode::Blend;
-                // material.base_color.set_a(0.1);
                 commands.entity(child).insert(materials.add(StandardMaterial {
                     alpha_mode: AlphaMode::Blend,
                     base_color: Color::Rgba{red:1., blue:1., green:1., alpha:0.3},
